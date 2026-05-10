91c816917597cc64c8efd7d58dc0c3b5cf973003
lshlsh98 <lsh980324@naver.com>
2026-05-10 12:59:12 +0900

    clone_back

---
+++ deli2matching/src/main/java/com/example/deli2matching/Deli2matchingApplication.java
@@ -0,0 +1,13 @@
+package com.example.deli2matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deli2matchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deli2matchingApplication.class, args);
+	}
+
+}
---
+++ deli2matching/src/test/java/com/example/deli2matching/Deli2matchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deli2matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deli2matchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
